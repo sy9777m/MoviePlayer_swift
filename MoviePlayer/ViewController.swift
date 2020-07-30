fc45d0d9a6e7f5aa5e27103386508423f5aa3560
Siyun Min <sy9777m@naver.com>
2020-07-30 22:39:25 +0900

    init

---
+++ MoviePlayer/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  MoviePlayer
+//
+//  Created by Siyun Min on 2020/07/30.
+//  Copyright © 2020 Siyun Min. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnPlayInternalMovie(_ sender: UIButton) {
+        let filePath:String? = Bundle.main.path(forResource: "example", ofType: "mp4")
+        let url = NSURL(fileURLWithPath: filePath!)
+        
+        playVideo(url: url)
+    }
+    
+    @IBAction func btnPlayExternalMovie(_ sender: UIButton) {
+        let url = NSURL(string: "https://dl.dropboxusercontent.com/s/e38auz050w2mvud/Fireworks.mp4")!
+        
+        playVideo(url: url)
+        
+    }
+    
+    private func playVideo(url: NSURL) {
+        let playerController = AVPlayerViewController()
+        
+        let player = AVPlayer(url: url as URL)
+        playerController.player = player
+        
+        self.present(playerController, animated: true) {
+            player.play()
+        }
+    }
+}
